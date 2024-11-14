979906f285ee609e925914be6999669ee24d8d73
Jason Cameron <jasoncameron246@gmail.com>
2024-11-14 18:25:54 -0500

    code refactor to have comments for each function, and better code reuabality.

--- main.go
+++ main.go
@@ -8,26 +8,43 @@ import (
 	"strings"
 )
 
+var taskItems = []string{
+	"Watch GoLang crash course.",
+	"Build SSG project",
+	"Reward myself"}
+
+var userOption string
+
 func main() {
-	var taskItems = []string{
-		"Watch GoLang crash course.",
-		"Build SSG project",
-		"Reward myself"}
+	mainProgram()
+}
 
-	var userOption string
-
+/*
+The mainProgram function executes the main logic of the todo list.
+*/
+func mainProgram() {
 	printTasks(taskItems)
 	fmt.Println()
+
 	fmt.Println("Here are all the items on your todo list. What would you like to do next? add a new item or remove and existing one ? (add, delete)")
+
 	fmt.Scanln(&userOption)
+
 	switch userOption {
 	case "add":
 		for {
 			fmt.Println("Please enter a new item. Type 'end' to exit!:")
 			reader := bufio.NewReader(os.Stdin)
 			text, _ := reader.ReadString('\n')
+
+			// Trim whitespace and newline chars.
+			text = strings.TrimSpace(text)
+			if "end" == text {
+				mainProgram()
+			}
 			taskItems = addTask(taskItems, text)
 			printTasks(taskItems)
+
 		}
 	case "delete":
 		for {
@@ -43,13 +60,18 @@ func main() {
 			if err != nil {
 				fmt.Println("Please enter a valid number. Type 'end' to exit!:")
 			}
+			if "end" == input {
+				mainProgram()
+			}
 			taskItems = removeTask(taskItems, num)
 			printTasks(taskItems)
 		}
 	}
-
 }
 
+/*
+printTask function takes in a slice containing strings, and prints them to the user.
+*/
 func printTasks(taskItems []string) {
 	fmt.Println("######## List of Tasks ########")
 	for index, task := range taskItems {
@@ -57,11 +79,17 @@ func printTasks(taskItems []string) {
 	}
 }
 
+/*
+addTask function takes the existing taskItems slice, and them add the new task item to it.
+*/
 func addTask(taskItems []string, newTask string) []string {
 	var updatedTaskItems = append(taskItems, newTask)
 	return updatedTaskItems
 }
 
+/*
+remmoveTask function removes a task from slice, based on the number of the task entered.
+*/
 func removeTask(taskItems []string, itemNumber int) []string {
 	var updatedItemItems = append(taskItems[:itemNumber-1], taskItems[itemNumber:]...)
 	return updatedItemItems
